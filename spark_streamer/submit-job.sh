2f7e9c86fa87258fd36452545d5833c77e029c5b
Prashant Kumar Thakur <38733726+prashantkthakur@users.noreply.github.com>
2019-04-30 18:02:51 -0600

    Update submit-job.sh

--- spark_streamer/submit-job.sh
+++ spark_streamer/submit-job.sh
@@ -3,4 +3,4 @@
 # Need to run the publisher
 #python ../codes/push_tweets.py
 
-spark-submit --packages org.apache.spark:spark-streaming-kafka-0-10_2.11:2.3.0 --class SparkConsumer build/libs/kafka-spark-streamer-1.0.jar
+spark-submit --packages org.apache.spark:spark-streaming-kafka-0-10_2.11:2.3.0,com.googlecode.json-simple:json-simple:1.1.1 --class SparkConsumer build/libs/kafka-spark-streamer-1.0.jar
