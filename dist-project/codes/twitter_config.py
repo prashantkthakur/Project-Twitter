e28cbf7f9b62bd6d7e32d029c3cfa5a21c3954cb
Prashant Kumar Thakur <38733726+prashantkthakur@users.noreply.github.com>
2019-05-13 15:12:33 -0600

    Update twitter_config.py

--- dist-project/codes/twitter_config.py
+++ dist-project/codes/twitter_config.py
@@ -2,7 +2,7 @@
 #These information comes from Twitter API
 #Create a Twitter Account and Get These Information from apps.twitter.com
 
-consumer_key = 'lncre7iHgWzn1FALOwD739f3c'
-consumer_secret = 'v8My6wEMvXRApUkWRHDNKdxbGd8kf84lulBldqLnsC1sIM4loK'
-access_token = '919589727753216007-JiPL32eR0Cx6H4dmr8wlfMik8Sns23N'
-access_secret = 'rMvaDpgSbgX5vCIyhQztEmJ3JZA65Kix2pK8GZmLyscbf'
+consumer_key = 'lncre7iHxxxxxFALOwD739f3c'
+consumer_secret = 'v8My6wEMvXRApUkWRHDNxxxxxxxx4lulBldqLnsC1sIM4loK'
+access_token = '91958xxxxxx3216007-JiPL32eR0CxxxxxxMik8Sns23N'
+access_secret = 'rMvaDpgSbgX5vCIyhQztxxxxxxxxix2pK8GZmLyscbf'
